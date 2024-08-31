c160d5630b1253f066fb64d86e220895fecfb523
rohitpahujawork <rohitpahuja.work@gmail.com>
2024-08-31 22:45:59 +0530

    fixed some minor change.

--- src/app/components/latestnews/latestnews.component.ts
+++ src/app/components/latestnews/latestnews.component.ts
@@ -14,38 +14,46 @@ export class LatestnewsComponent {
       image: 'assets/pexels-rodnae-productions-6936384 1.jpg',
       category: 'COMPANY',
       title: 'We can blend colors multiple ways, the most common',
-      styling:'border-radius: 15px; box-shadow: 20px 20px 0px'
+      styling:'border-radius: 15px; box-shadow: 20px 20px 0px #FFB6C1'
     },
     {
       image: 'assets/pexels-rodnae-productions-6936384 2.jpg',
-
       category: 'MARKETING EVENT',
-      title: 'How To Blow Through Capital At An Incredible Rate'
+      title: 'How To Blow Through Capital At An Incredible Rate',
+      styling:'border-radius: 15px; box-shadow: 20px 20px 0px #ADD8E6'
+
     },
     {
       image: 'assets/pexels-rodnae-productions-6936384 3.jpg',
-
       category: 'CUSTOMER SERVICES',
-      title: 'Design Studios That Everyone Should Know About?'
+      title: 'Design Studios That Everyone Should Know About?',
+      styling:'border-radius: 15px; box-shadow: 20px 20px 0px #FFB6C1'
     },
     {
       image: 'assets/pexels-rodnae-productions-6936384 4.jpg',
-
       category: 'COMPANY',
-      title: 'We can blend colors multiple ways, the most common'
+      title: 'We can blend colors multiple ways, the most common',
+      styling:'border-radius: 15px; box-shadow: 20px 20px 0px #ADD8E6'
+
     },
     {
       image: 'assets/pexels-rodnae-productions-6936384 5.jpg',
-
       category: 'MARKETING EVENT',
-      title: 'How To Blow Through Capital At An Incredible Rate'
+      title: 'How To Blow Through Capital At An Incredible Rate',
+      styling:'border-radius: 15px; box-shadow: 20px 20px 0px #FFB6C1'
     },
     {
       image: 'assets/pexels-rodnae-productions-6936384 6.jpg',
-
       category: 'CUSTOMER SERVICES',
-      title: 'Design Studios That Everyone Should Know About?'
+      title: 'Design Studios That Everyone Should Know About?',
+      styling:'border-radius: 15px; box-shadow: 20px 20px 0px #ADD8E6'
     }
     // Add more cards here if needed
   ];
+
+  getStyle(styling: string, property: string): string {
+    const styles = styling.split(';').map(style => style.split(':'));
+    const styleMap = new Map(styles.map(([key, value]) => [key.trim(), value.trim()]));
+    return styleMap.get(property) || '';
+  }
 }
