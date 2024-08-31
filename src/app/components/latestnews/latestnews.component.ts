8f11c004205e1a2e2f98239a39335879ec7464ef
rohitpahujawork <rohitpahuja.work@gmail.com>
2024-08-31 23:19:13 +0530

    fixed some minor cahnges

--- src/app/components/latestnews/latestnews.component.ts
+++ src/app/components/latestnews/latestnews.component.ts
@@ -14,39 +14,39 @@ export class LatestnewsComponent {
       image: 'assets/pexels-rodnae-productions-6936384 1.jpg',
       category: 'COMPANY',
       title: 'We can blend colors multiple ways, the most common',
-      styling:'border-radius: 15px; box-shadow: 20px 20px 0px #FFB6C1'
+      styling:'border-radius: 15px; box-shadow: 20px 20px 0px #fff0ff'
     },
     {
       image: 'assets/pexels-rodnae-productions-6936384 2.jpg',
       category: 'MARKETING EVENT',
       title: 'How To Blow Through Capital At An Incredible Rate',
-      styling:'border-radius: 15px; box-shadow: 20px 20px 0px #ADD8E6'
+      styling:'border-radius: 15px; box-shadow: 20px 20px 0px #f0f8ff'
 
     },
     {
       image: 'assets/pexels-rodnae-productions-6936384 3.jpg',
       category: 'CUSTOMER SERVICES',
       title: 'Design Studios That Everyone Should Know About?',
-      styling:'border-radius: 15px; box-shadow: 20px 20px 0px #FFB6C1'
+      styling:'border-radius: 15px; box-shadow: 20px 20px 0px  #fff0ff'
     },
     {
       image: 'assets/pexels-rodnae-productions-6936384 4.jpg',
       category: 'COMPANY',
       title: 'We can blend colors multiple ways, the most common',
-      styling:'border-radius: 15px; box-shadow: 20px 20px 0px #ADD8E6'
+      styling:'border-radius: 15px; box-shadow: 20px 20px 0px #f0f8ff'
 
     },
     {
       image: 'assets/pexels-rodnae-productions-6936384 5.jpg',
       category: 'MARKETING EVENT',
       title: 'How To Blow Through Capital At An Incredible Rate',
-      styling:'border-radius: 15px; box-shadow: 20px 20px 0px #FFB6C1'
+      styling:'border-radius: 15px; box-shadow: 20px 20px 0px  #fff0ff'
     },
     {
       image: 'assets/pexels-rodnae-productions-6936384 6.jpg',
       category: 'CUSTOMER SERVICES',
       title: 'Design Studios That Everyone Should Know About?',
-      styling:'border-radius: 15px; box-shadow: 20px 20px 0px #ADD8E6'
+      styling:'border-radius: 15px; box-shadow: 20px 20px 0px #f0f8ff'
     }
     // Add more cards here if needed
   ];
