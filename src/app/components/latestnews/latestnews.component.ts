2722c79150f3f8b21bc9d18ce273cfdc58830737
rohitpahujawork <rohitpahuja.work@gmail.com>
2024-08-31 19:29:47 +0530

    add latestnews feature component.

--- src/app/components/latestnews/latestnews.component.ts
+++ src/app/components/latestnews/latestnews.component.ts
@@ -11,32 +11,38 @@ import { Component } from '@angular/core';
 export class LatestnewsComponent {
   cards = [
     {
-      image: 'path_to_image1.jpg',
+      image: 'assets/pexels-rodnae-productions-6936384 1.jpg',
       category: 'COMPANY',
-      title: 'We can blend colors multiple ways, the most common'
+      title: 'We can blend colors multiple ways, the most common',
+      styling:'border-radius: 15px; box-shadow: 20px 20px 0px'
     },
     {
-      image: 'path_to_image2.jpg',
+      image: 'assets/pexels-rodnae-productions-6936384 2.jpg',
+
       category: 'MARKETING EVENT',
       title: 'How To Blow Through Capital At An Incredible Rate'
     },
     {
-      image: 'path_to_image3.jpg',
+      image: 'assets/pexels-rodnae-productions-6936384 3.jpg',
+
       category: 'CUSTOMER SERVICES',
       title: 'Design Studios That Everyone Should Know About?'
     },
     {
-      image: 'path_to_image1.jpg',
+      image: 'assets/pexels-rodnae-productions-6936384 4.jpg',
+
       category: 'COMPANY',
       title: 'We can blend colors multiple ways, the most common'
     },
     {
-      image: 'path_to_image2.jpg',
+      image: 'assets/pexels-rodnae-productions-6936384 5.jpg',
+
       category: 'MARKETING EVENT',
       title: 'How To Blow Through Capital At An Incredible Rate'
     },
     {
-      image: 'path_to_image3.jpg',
+      image: 'assets/pexels-rodnae-productions-6936384 6.jpg',
+
       category: 'CUSTOMER SERVICES',
       title: 'Design Studios That Everyone Should Know About?'
     }
